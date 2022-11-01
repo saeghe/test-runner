b02a831364dd61c63b83d73d3f3d64fedb214241
Morteza <morteza@protonmail.com>
2022-11-01 15:19:48 +0100

    Use getcwd instead of SERVER['PWD'] since it is not supported in windows

--- Tests/RunTest.php
+++ Tests/RunTest.php
@@ -5,7 +5,7 @@ namespace Tests\RunTest;
 test(
     title: 'it should run tests in given directory',
     case: function () {
-        exec($_SERVER['PWD'] . '/run --directory=OtherTestDirectory', $output);
+        exec('php ' . getcwd() . DIRECTORY_SEPARATOR . 'run --directory=OtherTestDirectory', $output);
 
         assert(str_contains($output[0], "✅ it should pass the test case"));
         assert(3 === count($output));
